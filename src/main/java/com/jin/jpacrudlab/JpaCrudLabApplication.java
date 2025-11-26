6c923bf8dc0fd711b934639debfe55b7cf4c5507
Kim Jinhyeon <jinhyun7675@gmail.com>
2025-11-27 01:30:58 +0900

    Initial commit

---
+++ src/main/java/com/jin/jpacrudlab/JpaCrudLabApplication.java
@@ -0,0 +1,13 @@
+package com.jin.jpacrudlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCrudLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaCrudLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jin/jpacrudlab/JpaCrudLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jin.jpacrudlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaCrudLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
